1246794c74cf65d941054e2d0c19d5e730d08d21
JLee <thishereisjenn@gmail.com>
2021-12-18 16:08:27 -0800

    tying together not working

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+    res.json(noteList);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now listening on ${PORT}...`);
+});
