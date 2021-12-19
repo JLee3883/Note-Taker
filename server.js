60073154870cb2d05265adf33238712c24a29899
JLee <thishereisjenn@gmail.com>
2021-12-18 17:20:32 -0800

    finally got rid of bugs to link all pages

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
     res.json(noteList);
