1169536c5be6f35edd76f3c94c07471adefb7cbd
JLee <thishereisjenn@gmail.com>
2021-12-18 22:56:38 -0800

    fixed posting issue

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
-const path = require("path");
+
+const noteList = require("./db/db.json");
+
 const app = express();
 
-app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -16,9 +19,23 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
 app.get("/api/notes", (req, res) => {
     res.json(noteList);
 });
+
+app.post("/api/notes", (req, res) => {
+    req.body.id = noteList.length.toString();
+    const newNote = req.body;
+    createNewNote(newNote, noteList);
+    res.json(newNote);
+});
+
+function createNewNote(body, notes) {
+    const note = body;
+    notes.push(note);
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(noteList, null, 2));
+}
 
 app.listen(PORT, () => {
     console.log(`API server now listening on ${PORT}...`);
